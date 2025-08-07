2f18bef200a747411b8d3883649803be0286aa19
Diego Bran <diegobran@umes.edu.gt>
2025-08-07 15:21:08 -0600

    Se agregan los JSONs de Mock-Data

---
+++ API_Ferreteria_DB/server.js
@@ -0,0 +1,25 @@
+//Imports de dependencias
+import express from 'express'; 
+//import routes from './routes/index.route.js';
+
+// Importación de rutas
+// const productRoutes = require('./routes/product.routes.js');
+// const supplierRoutes =require('./routes/supplier.routes.js');
+
+// Inicialización de la aplicación Express
+const app = express();
+const port = 3001;
+app.use(express.json());
+
+// Configuración de las rutas
+// app.use('/api/products', productRoutes);
+// app.use('/api/suppliers', supplierRoutes);
+
+//Error del servidor si falla
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Error del servidor')});
+
+// Inicio del servidor
+app.listen(port, () => console.log('Servidor Iniciado'));
+
